cb571c1ff09cb0f1b41a88b6f5e5c9839bd57c5b
mathis-ynov <mathis.frenais@ynov.com>
2021-04-27 22:54:29 +0200

    increase the cost of towers

--- scripts/BuildingUI.gd
+++ scripts/BuildingUI.gd
@@ -2,6 +2,7 @@ extends HBoxContainer
 
 export (NodePath) var building_item
 var main
+export var labels_cost = []
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -12,6 +13,8 @@ func _ready():
 		button.get_node("VBoxContainer/Label").text = main.towers[index].name
 		button.get_node("VBoxContainer/HBoxContainer/Label").text = String(main.towers[index].cost)
 		button.connect("pressed", self, "_on_button", [index])
+		labels_cost.append(button.get_node("VBoxContainer/HBoxContainer/Label"))
+		print_debug(labels_cost)
 	for indexWizard in range(main.medics.size()):
 		var button = get_node(building_item).duplicate()
 		add_child(button)
@@ -27,3 +30,12 @@ func _on_button(index):
 func _on_button_medic(indexWizard):
 	main.medic_index = indexWizard
 	main.type_building = 'medic'
+	
+func calculate_cost(label_cost, nb_entities):
+	var cost = float(label_cost.text) * pow(1.5,nb_entities)
+	cost = round(cost/10)*10
+	return str(cost)
+	
+func update_cost(nb_entities):
+	for label_cost in labels_cost:
+		label_cost.text = calculate_cost(label_cost, nb_entities)
